82297c2cdc6a452fb840d73eacd0b3a87ec32396
qasimjalal4 <qjalal2005@gmail.com>
2026-09-20 15:18:35 +0500

    move the task controllers to controllers

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,104 +1,17 @@
 import express from 'express'
 import { readTasks, writeTask } from '../utils/TaskFile.js'
+import { getTasks, createTask, deleteTask} from '../controllers/taskControllers.js'
+
+
 
 const router = express.Router()
 
 
+router.get('/', getTasks)  
 
+router.post('/', createTask)
 
-router.get('/', async (req,res) => {
-
-  const { search, priority, status } = req.query
-
-  const tasks = await readTasks()
-
-  let filteredTasks = tasks
-
-  if(search) {
-    filteredTasks = filteredTasks.filter(task => task.title.toLowerCase().includes(search.toLowerCase()))
-  }
-
-
-  if(priority) {
-    filteredTasks = filteredTasks.filter(task => task.priority === priority)
-  }
-
-
-  if(status) {
-    filteredTasks = filteredTasks.filter(task => task.status === status)
-  }
-
-
-  res.status(200).json({
-    success: true,
-    data: filteredTasks
-  })
-
-})  
-
-
-
-
-router.post('/', async (req,res) => {
-
-
-  const { title, desc, due, priority} = req.body
-
-
-  const tasks = await readTasks()
-
-
-  const newTask = {
-    id: tasks.length > 0 ? Math.max(...tasks.map((task) => task.id)) + 1 : 1,
-    title: title,
-    desc: desc,
-    due: due,
-    created: new Date().toISOString().split('T')[0],
-    priority: priority,
-    done: false
-  }
-
-  tasks.push(newTask)
-
-  await writeTask(tasks)
-
-
-  res.status(201).json({
-    success: true,
-    data: newTask
-  })
-
-})
-
-
-router.delete('/:id',  async (req,res) => {
-
-  const id = Number(req.params.id)
-
-  const tasks = await readTasks()
-
-
-  const taskIndex = tasks.findIndex(task => task.id === id) 
-
-  if (taskIndex === -1) {
-    return res.status(404).json({
-      success: false,
-      message: 'task not found!'
-    })
-
-  }
-
-
- const deletedTask = tasks.splice(taskIndex,1)
-
- await writeTask(tasks)
-
- res.status(200).json({
-  success: true,
-  data: deletedTask
- })
-
-})
+router.delete('/:id', deleteTask)
 
 
 
