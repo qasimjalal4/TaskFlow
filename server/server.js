8c858a575121e676c66dab454a6a3782f029dec3
qasimjalal4 <qjalal2005@gmail.com>
2026-09-20 15:10:14 +0500

    move task routes to router

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import path from 'path'
 import fs from 'fs/promises'
 import { fileURLToPath } from 'url'
- 
+import taskRouter from './routes/taskRoutes' 
 
 const app = express()
 
@@ -13,16 +13,12 @@ app.use(express.json())
 
 
 const __filename = fileURLToPath(import.meta.url)
-
 const __dirname = path.dirname(__filename)
-
-
 
 const filePath = path.join(__dirname,'data','data.json')
 
 
-
-const readTasks = async () => {
+export const readTasks = async () => {
 
   const tasks = await fs.readFile(filePath,'utf-8')
 
@@ -32,7 +28,7 @@ const readTasks = async () => {
 
 
 
-const writeTask = async (tasks) => {
+export const writeTask = async (tasks) => {
 
   await fs.writeFile(filePath,JSON.stringify(tasks,null,2))
 
@@ -40,104 +36,11 @@ const writeTask = async (tasks) => {
 
 
 
-app.get('/tasks', async (req,res) => {
+app.use('/tasks', taskRouter)
 
-  const { search, priority, status } = req.query
+app.use('/tasks', taskRouter)
 
-  const tasks = await readTasks()
-
-  let filteredTasks = tasks
-
-  if(search) {
-
-    filteredTasks = filteredTasks.filter(task => task.title.toLowerCase().includes(search.toLowerCase()))
-
-  }
-
-  if(priority) {
-
-    filteredTasks = filteredTasks.filter(task => task.priority === priority)
-
-  }
-
-
-  if(status) {
-
-    filteredTasks = filteredTasks.filter(task => task.status === status)
-
-  }
-
-
-  res.status(200).json({
-    success: true,
-    data: filteredTasks
-
-  })
-
-})
-
-
-
-app.post('/tasks', async (req,res) => {
-
-
-  const { title, desc, due, priority} = req.body
-
-
-  const tasks = await readTasks()
-
-
-  const newTask = {
-    id: tasks.length > 0 ? Math.max(...tasks.map((task) => task.id)) + 1 : 1,
-    title: title,
-    desc: desc,
-    due: due,
-    created: new Date().toISOString().split('T')[0],
-    priority: priority,
-    done: false
-  }
-
-  tasks.push(newTask)
-
-  await writeTask(tasks)
-
-
-  res.status(201).json({
-    success: true,
-    data: newTask
-  })
-
-})
-
-
-app.delete('/tasks/:id', async (req,res) => {
-
-  const id = Number(req.params.id)
-
-  const tasks = await readTasks()
-
-
-  const taskIndex = tasks.findIndex(task => task.id === id) 
-
-  if (taskIndex === -1) {
-    return res.status(404).json({
-      success: false,
-      message: 'task not found!'
-    })
-
-  }
-
-
-  tasks.splice(taskIndex,1)
-
- await writeTask(tasks)
-
- res.status(200).json({
-  success: true,
-  data: tasks 
- })
-
-})
+app.use('/tasks/:id', taskRouter)
 
 
 
