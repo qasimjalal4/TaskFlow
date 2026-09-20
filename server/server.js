e3abff4fd8d41724567255bea89098905166c59e
qasimjalal4 <qjalal2005@gmail.com>
2026-09-20 14:56:26 +0500

    use GET route to add task search , priority and status filtering

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ import express from 'express'
 import path from 'path'
 import fs from 'fs/promises'
 import { fileURLToPath } from 'url'
-import validateTask from './middlewares/validateTask.js'
+ 
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
-
+app.use(express.json())
 
 
 
@@ -37,6 +37,45 @@ const writeTask = async (tasks) => {
   await fs.writeFile(filePath,JSON.stringify(tasks,null,2))
 
 }
+
+
+
+app.get('/tasks', async (req,res) => {
+
+  const { search, priority, status } = req.query
+
+  const tasks = await readTasks()
+
+  let filteredTasks = tasks
+
+  if(search) {
+
+    filteredTasks = filteredTasks.filter(task => task.title.toLowerCase().includes(search.toLowerCase()))
+
+  }
+
+  if(priority) {
+
+    filteredTasks = filteredTasks.filter(task => task.priority === priority)
+
+  }
+
+
+  if(status) {
+
+    filteredTasks = filteredTasks.filter(task => task.status === status)
+
+  }
+
+
+  res.status(200).json({
+    success: true,
+    data: filteredTasks
+
+  })
+
+})
+
 
 
 
